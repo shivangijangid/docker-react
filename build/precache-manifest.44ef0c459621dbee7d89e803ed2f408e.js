5fcd919a5f3c62d8b5d431df9b0cc742c77f6fb5
Shivangi Jangid <shivangi@Shivangis-MacBook-Pro.local>
2019-03-22 21:42:13 +0530

    react with tests

---
+++ build/precache-manifest.44ef0c459621dbee7d89e803ed2f408e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab063f5e0bd225b7dfa1",
+    "url": "/static/js/main.cbadf30c.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "ab063f5e0bd225b7dfa1",
+    "url": "/static/css/main.1339d66c.chunk.css"
+  },
+  {
+    "revision": "de9b69223d6432603365ffbfd7551c9c",
+    "url": "/index.html"
+  }
+];
